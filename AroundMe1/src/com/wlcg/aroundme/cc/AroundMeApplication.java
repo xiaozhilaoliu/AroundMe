55fad5306c920c4336905f294265e96159f734e3
xiaozhilaoliu <517662315@qq.com>
2014-03-27 10:41:51 +0800

    修改字符串

--- AroundMe1/src/com/wlcg/aroundme/cc/AroundMeApplication.java
+++ AroundMe1/src/com/wlcg/aroundme/cc/AroundMeApplication.java
@@ -6,7 +6,7 @@ import android.app.Application;
 
 public class AroundMeApplication extends Application{
 	public static String temp_data = "";
-	public static String huminity_data = "";
+	public static String huminity_data = "hahahaha";
 	public static String air_data = "";
 	@Override
 	public void onCreate() {
